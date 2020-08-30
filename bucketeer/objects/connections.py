b29b269cb84e2205097220952e3e983ee6b1a4a4
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-08-29 21:46:12 -0400

    update type hints for Connection

--- bucketeer/objects/connections.py
+++ bucketeer/objects/connections.py
@@ -1,6 +1,8 @@
+import typing
 
-from bucketeer.objects.key import Key
-
+if typing.TYPE_CHECKING:
+   from bucketeer.objects.key import Key
+   from bucketeer.exceptions import KeyRetrievalFailure
 
 class Connection:
     """ Base object to be inherited for custom connections """
@@ -11,5 +13,5 @@ class Connection:
     def _remove_counter_to_key(self, key:str, service: str) -> bool:
         raise NotImplemented 
 
-    def get_lowest_used_key(self, service: str) -> dict:
+    def get_lowest_used_key(self, service: str) -> "typing.Union[Key, typing.Union[bool, KeyRetrievalFailure]]":
         raise NotImplemented
